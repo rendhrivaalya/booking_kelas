1e42879d7819cf90ab2df7b81bc895f4fbbae565
alya <rendhrivaalya@gmail.com>
2026-01-22 09:09:26 +0700

    fix: mock dependencies in auth tests

--- user-service/src/app.controller.spec.ts
+++ user-service/src/app.controller.spec.ts
@@ -15,8 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "User Service is running"', () => {
+      // Ubah baris di bawah ini agar sesuai dengan return AppService
+      expect(appController.getHello()).toBe('User Service is running'); 
     });
   });
 });
--- user-service/src/auth/auth.service.spec.ts
+++ user-service/src/auth/auth.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {}, // Mock database
+        },
+        {
+          provide: AmqpConnection,
+          useValue: {}, // Mock RabbitMQ
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
